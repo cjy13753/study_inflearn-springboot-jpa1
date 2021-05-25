7bffd14a1272a29c37bd56a8479e95ce7b5b4c42
Jun Choi <cjy13753@gmail.com>
2021-05-25 11:48:37 +0900

    Test MemberService for member join and duplicate member join check - [회원 기능 테스트]

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+//    @Autowired EntityManager em;
+
+    @Test
+//    @Rollback(false) // 이거 하면 실제로 DB에 박혀버림. 눈으로 결과 확인 가능
+    public void 회원가입() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setName("kim");
+        //When
+        Long saveId = memberService.join(member);
+//        em.flush(); // insert query 나가는 거 볼 수 있음(대신 @Transactional에 의해 rollback 됨)
+        //Then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); // 예외가 발생해야 한다!!
+
+        //then
+//        fail("에외가 발생해야 하는데 발생하지 않았다."); // 위에 expected = ... 있어서 필요 없어짐
+
+    }
+}
